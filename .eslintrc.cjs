9dd390cf6cb64f94607f249cce847a2900c06d33
abdo-elmorsi <abdelrahmandiv@gmail.com>
2025-01-10 00:48:14 +0200

    esl

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,6 +3,7 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    node: true, // Added for Node.js support since Prisma may require it
   },
   extends: [
     'eslint:recommended',
@@ -10,8 +11,8 @@ module.exports = {
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
     'plugin:jsx-a11y/recommended',
-    'next/core-web-vitals', // Add Next.js with core web vitals checks
-    'prettier', // Add Prettier for code formatting
+    'next/core-web-vitals', // Next.js core web vitals checks
+    'prettier', // Prettier for consistent code formatting
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: {
@@ -20,20 +21,20 @@ module.exports = {
   },
   settings: {
     react: {
-      version: 'detect',
+      version: 'detect', // Automatically detect the React version
     },
   },
-  plugins: ['react', 'jsx-a11y'],
+  plugins: ['react', 'jsx-a11y', 'prettier'], // Added Prettier as a plugin
   rules: {
-    'react/prop-types': 'off', // Turn off if using TypeScript or PropTypes are not used
-    'react/jsx-uses-react': 'off', // Disable as React 17+ and Next.js handle this automatically
-    'react/jsx-uses-vars': 'error', // Ensures variables used in JSX are defined
-    'react/display-name': 'off', // Avoid noisy display name warnings in certain cases
-    'react-hooks/exhaustive-deps': 'warn', // Warn if useEffect/useCallback dependencies are missing
-    'jsx-a11y/anchor-is-valid': 'off', // Disabled due to conflict with Next.js Link component
-    'no-console': 'warn', // Warn about console logs in production
-    'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }], // Warn about unused variables, ignore underscore-prefixed ones
-    'no-undef': 'error', // Throw an error when using an undefined variable or function
-    'prettier/prettier': ['warn', { singleQuote: true, trailingComma: 'all' }], // Prettier rules for code style
+    'react/prop-types': 'off', // Turn off PropTypes if using TypeScript
+    'react/jsx-uses-react': 'off', // Disable as React 17+ and Next.js manage this automatically
+    'react/jsx-uses-vars': 'error', // Ensure variables used in JSX are defined
+    'react/display-name': 'off', // Avoid display name warnings
+    'react-hooks/exhaustive-deps': 'warn', // Warn about missing dependencies in hooks
+    'jsx-a11y/anchor-is-valid': 'off', // Disabled due to Next.js Link component conflicts
+    'no-console': 'warn', // Warn about console logs
+    'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }], // Ignore underscore-prefixed unused variables
+    'no-undef': 'error', // Error on undefined variables
+    'prettier/prettier': ['warn', { singleQuote: true, trailingComma: 'all' }], // Prettier rules for consistent formatting
   },
 };
